d6634f367da8d770792dc448b686bd081ed99868
Dat Pham <faceless5879@gmail.com>
2025-03-07 08:04:13 +0900

    feat: add order test

---
+++ internal/orders/domain/order/order_status_test.go
@@ -0,0 +1,27 @@
+package order_test
+
+import (
+	"github.com/faceless5879/mono-go-es-ddd/internal/orders/domain/order"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOrder_NewOrderStatusFromString(t *testing.T) {
+	t.Parallel()
+	orderStatus := "PENDING"
+
+	os, err := order.NewOrderStatusFromString(orderStatus)
+	require.NoError(t, err)
+
+	assert.Equal(t, os.String(), orderStatus)
+}
+
+func TestOrder_NewOrderStatusFromString_invalid(t *testing.T) {
+	t.Parallel()
+	orderStatus := "invalid status"
+
+	_, err := order.NewOrderStatusFromString(orderStatus)
+	assert.Error(t, err)
+}
---
+++ internal/orders/domain/order/order_test.go
@@ -0,0 +1,37 @@
+package order_test
+
+import (
+	"github.com/faceless5879/mono-go-es-ddd/internal/orders/domain/order"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOrder_Init(t *testing.T) {
+	t.Parallel()
+	item, _ := order.NewOrderItem("123", 2)
+	orderItems := []order.OrderItem{item}
+	deliveryAddress, _ := order.NewDeliveryAddress("Test", "Test Addr")
+	userUUID := "user id"
+	orderID := uuid.New()
+
+	newOrder, _ := order.NewOrder(orderID)
+	newOrder.Init(userUUID, orderItems, deliveryAddress)
+	events := newOrder.GetUncommitedEvents()
+
+	assert.Equal(t, newOrder.ID(), orderID)
+	assert.Equal(t, newOrder.DeliveryAddress(), deliveryAddress)
+	assert.Equal(t, newOrder.UserID(), userUUID)
+
+	// Check events
+	assert.Len(t, events, 1)
+	if len(events) > 0 {
+		e, _ := events[0].(order.OrderCreatedEvent)
+		assert.Equal(t, order.OrderCreated, e.EventType())
+		assert.Equal(t, orderID, e.OrderID)
+		assert.Equal(t, userUUID, e.UserUUID)
+		assert.Equal(t, newOrder.DeliveryAddress().ReceiverName(), e.ReceiverName)
+		assert.Equal(t, newOrder.DeliveryAddress().Address(), e.DeliveryAddress)
+		assert.Equal(t, orderItems, e.OrderItems)
+	}
+}
