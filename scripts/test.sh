09d2f7c44b144b22587f502bcd81066b3091d44c
Dat Pham <faceless5879@gmail.com>
2025-03-07 08:00:58 +0900

    fix: edit scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,6 @@
 set -e
 
 readonly service="$1"
-readonly env_file="$2"
 
 cd "./internal/$service"
-env $(cat "../../.env" "../../$env_file" | grep -Ev '^#' | xargs) go test -count=1 -p=8 -parallel=8 -race ./...
+go test -count=1 -p=8 -parallel=8 ./...
