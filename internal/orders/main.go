3c184119bd42fb08dab0610cb58778ca08da9884
ファム　ダト <dat.pham@mobilus.co.jp>
2025-05-12 10:28:32 +0900

    rename package and dir

--- internal/orders/main.go
+++ internal/orders/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/faceless5879/mono-go-es-ddd/internal/common/logs"
-	"github.com/faceless5879/mono-go-es-ddd/internal/common/servers"
+	"github.com/faceless5879/mono-go-es-ddd/internal/common/server"
 	"github.com/faceless5879/mono-go-es-ddd/internal/orders/ports"
 	"github.com/faceless5879/mono-go-es-ddd/internal/orders/service"
 	"github.com/go-chi/chi/v5"
@@ -13,7 +13,7 @@ import (
 func main() {
 	logs.Init()
 	application := service.NewApplication()
-	servers.RunHTTPServer(func(router chi.Router) http.Handler {
+	server.RunHTTPServer(func(router chi.Router) http.Handler {
 		return ports.HandlerFromMux(
 			ports.NewHttpServer(application),
 			router,
